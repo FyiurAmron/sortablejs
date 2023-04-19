236cd20e4da665cf410622cfd5a2cf02fb72ee8f
Damien Regad <dregad@mantisbt.org>
2023-04-20 01:14:11 +0200

    Remove deprecated use of require_once(syntax.php)
    From the DokuWiki deprecation log:
    require(syntax.php) is deprecated. It was called from require() in
    .../sortablejs/syntax.php:17 Autoloading should be used instead!

--- syntax.php
+++ syntax.php
@@ -12,9 +12,6 @@
 // must be run within Dokuwiki
 if ( !defined( 'DOKU_INC' ) )
     die();
-if ( !defined( 'DOKU_PLUGIN' ) )
-    define( 'DOKU_PLUGIN', DOKU_INC.'lib/plugins/' );
-require_once(DOKU_PLUGIN.'syntax.php');
 //
 class syntax_plugin_sortablejs extends DokuWiki_Syntax_Plugin {
 
